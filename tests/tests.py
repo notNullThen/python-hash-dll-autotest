16ae8f476efba1fb2343966cf2f8245be64d2634
Jenő Pekarjuk <epekaryuk@outlook.com>
2025-07-08 18:58:29 +0200

    2 tests introduced

--- tests/tests.py
+++ tests/tests.py
@@ -1,19 +1,49 @@
 import time
-from tests.hash_wrapper import HashWrapper
-
+import pytest
+from hash_wrapper import HashWrapper
 
 hash_wrapper = HashWrapper()
 
-hash_wrapper.initialize()
-operation_id = hash_wrapper.hash_directory("./testData")
 
-while hash_wrapper.get_status(operation_id):
-    time.sleep(0.1)
+def get_directory_hash(path: str):
+    operation_id = hash_wrapper.hash_directory(path)
 
-line_ptr = hash_wrapper.read_next_log_line()
-result = line_ptr.value.decode("utf-8")
-hash_wrapper.free(line_ptr)
+    while hash_wrapper.get_status(operation_id):
+        time.sleep(0.1)
+    line_ptr = hash_wrapper.read_next_log_line()
 
-hash_wrapper.terminate()
+    return line_ptr
 
-print(result)
+
+def test_memory_clean_up():
+    hash_wrapper.initialize()
+
+    operation_id = hash_wrapper.hash_directory("./testData")
+
+    while hash_wrapper.get_status(operation_id):
+        time.sleep(0.1)
+
+    line_ptr = hash_wrapper.read_next_log_line()
+    result_value = line_ptr.value
+
+    print(result_value.decode("utf-8"))
+
+    hash_wrapper.free(line_ptr)
+
+    assert (
+        line_ptr.value is None
+    ), f'Memory was not freed up correctly! line_ptr.value is: "{line_ptr.value}"'
+
+    hash_wrapper.terminate()
+
+
+def test_multiple_hashes():
+    directoryPath = "./testData"
+
+    hash_wrapper.initialize()
+
+    result = get_directory_hash(directoryPath).value
+
+    print(result.decode("utf-8"))
+
+    hash_wrapper.terminate()
