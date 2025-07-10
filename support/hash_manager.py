97e2568c5ebcdc644432b3c5b9f4df2a5605ca3f
Jenő Pekarjuk <epekaryuk@outlook.com>
2025-07-10 09:58:00 +0200

    HashManager types

--- support/hash_manager.py
+++ support/hash_manager.py
@@ -4,53 +4,58 @@ from hash_wrapper import HashWrapper
 
 class HashManager:
     def __init__(self, hash_wrapper: HashWrapper):
-        self.wrapper = hash_wrapper
+        self._wrapper = hash_wrapper
 
     def initialize(self):
         print("Initializing the hash library...\n")
-        result = self.wrapper.HashInit()
+        result = self._wrapper.HashInit()
         self._check_for_error(result)
 
     def terminate(self):
         print("Terminating the hash library...\n")
-        result = self.wrapper.HashTerminate()
+        result = self._wrapper.HashTerminate()
         self._check_for_error(result)
 
     def hash_directory(self, directory):
         print(f"Hashing the contents of a '{directory}' directory...\n")
         operation_id = c_size_t()
-        result = self.wrapper.HashDirectory(directory.encode("utf-8"), byref(operation_id))
+        result = self._wrapper.HashDirectory(directory.encode("utf-8"), byref(operation_id))
         self._check_for_error(result)
 
         return operation_id.value
 
-    def read_next_log_line(self):
+    def read_next_log_line_and_free(self):
         print("Reading the next log line from the hash operation...\n")
 
         line_ptr = c_char_p()
-        result = self.wrapper.HashReadNextLogLine(byref(line_ptr))
-        self._check_for_error(result)
 
-        return line_ptr.value
+        next_log_line_result = self._wrapper.HashReadNextLogLine(byref(line_ptr))
+        self._check_for_error(next_log_line_result)
+
+        result = line_ptr.value
+
+        self._wrapper.HashFree(line_ptr)
+
+        return result
 
     def get_running_status(self, operation_id_value) -> bool:
         running = c_bool(True)
-        result = self.wrapper.HashStatus(c_size_t(operation_id_value), byref(running))
+        result = self._wrapper.HashStatus(c_size_t(operation_id_value), byref(running))
         self._check_for_error(result)
 
         return running.value
 
-    def stop(self, operation_id):
-        print(f"Stopping the '{operation_id}' operation...\n")
-        result = self.wrapper.HashStop(c_size_t(operation_id))
+    def stop(self, operation_id_value):
+        print(f"Stopping the '{operation_id_value}' operation...\n")
+        result = self._wrapper.HashStop(c_size_t(operation_id_value))
         self._check_for_error(result)
 
-    def free(self, pointer):
+    def free(self, pointer: c_char_p):
         print(f"Cleaning up pointer memory...\n")
 
-        self.wrapper.HashFree(pointer)
+        self._wrapper.HashFree(pointer)
 
     def _check_for_error(self, code):
         if code == 0:
             return
-        raise Exception(f"HASH ERROR: {self.wrapper.get_error_from_code(code)}")
+        raise Exception(f"HASH ERROR: {self._wrapper.get_error_from_code(code)}")
